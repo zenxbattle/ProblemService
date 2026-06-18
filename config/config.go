abcd789c731b181ef905e4a2dd60fe909baf6f7a
lt-aion <liju.thomas@aion.xyz>
2026-06-19 04:36:52 +0530

    refactor: construct mongo url from individual env vars

--- config/config.go
+++ config/config.go
@@ -1,6 +1,7 @@
 package configs
 
 import (
+	"fmt"
 	"log"
 	"os"
 
@@ -12,10 +13,9 @@ type Config struct {
 	UserGRPCPort   string
 	MongoDBURL     string
 	ProblemService string
-	NatsURL       string
+	NatsURL        string
 	RedisURL       string
-
-	Environment string
+	Environment    string
 }
 
 func LoadConfig() Config {
@@ -23,15 +23,21 @@ func LoadConfig() Config {
 	if err != nil {
 		log.Println("No .env file found, using defaults:", err)
 	}
+
+	mongoUser := getEnv("MONGO_USER", "admin")
+	mongoPassword := getEnv("MONGO_PASSWORD", "admin")
+	mongoHost := getEnv("MONGO_HOST", "mongodb.default.svc.cluster.local")
+	mongoPort := getEnv("MONGO_PORT", "27017")
+	mongoURL := fmt.Sprintf("mongodb://%s:%s@%s:%s", mongoUser, mongoPassword, mongoHost, mongoPort)
+
 	config := Config{
 		APIGATEWAYPORT: getEnv("APIGATEWAYPORT", "7000"),
 		UserGRPCPort:   getEnv("USERGRPCPORT", "50051"),
-		MongoDBURL:     getEnv("MONGODBURL", "mongodb://localhost:27017"),
+		MongoDBURL:     mongoURL,
 		ProblemService: getEnv("PROBLEMSERVICE", "50055"),
-		NatsURL:       getEnv("NATSURL", "nats://localhost:4222"),
+		NatsURL:        getEnv("NATSURL", "nats://localhost:4222"),
 		RedisURL:       getEnv("REDISURL", "localhost:6379"),
-
-		Environment: getEnv("ENVIRONMENT", "development"),
+		Environment:    getEnv("ENVIRONMENT", "development"),
 	}
 
 	// fmt.Println(config)
